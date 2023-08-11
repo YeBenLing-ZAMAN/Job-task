889bc0c91ae4f10b3b62da16be12de0e9448174d
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-08-11 20:56:53 +0600

    [update] filter and product

--- src/pages/FrontViewPages/pages/Products.js
+++ src/pages/FrontViewPages/pages/Products.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { products } from "../../../assets/ProductData/products";
-import ShowProducts from "../components/ShowProducts";
 import Button from "../../../components/Button";
 import { useSearchParams } from "react-router-dom";
 import InputSearch from "../../../components/Input/InputSearch";
@@ -12,33 +11,33 @@ import Filters from "../../../components/Filter";
 const Products = () => {
   const [showFilters, setShowFilters] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
-
   return (
     <div className="max-w-[80vw] mx-auto">
-      <div className="">
-        <div className="my-10">
-          <InputSearch
-            className="w-full p-2 text-xl border border-red-500 rounded"
-            placeholder="Search Your Product"
-          ></InputSearch>
-        </div>
-        <div className="w-full flex items-center gap-3 justify-start ">
-          <Button
-            name="Filters"
-            className="p-2 text-xl flex items-center gap-1"
-            onClick={() => setShowFilters((prev) => !prev)}
-          >
-            <CiBoxList />
-            Filters
-          </Button>
-          <div onClick={() => setSearchParams({})} className="clAllTxt">
-            Clear filters
-          </div>
+      <div className="my-10 text-center">
+        <InputSearch
+          className="w-full md:w-1/2 p-2 text-xl border border-gray-500 rounded"
+          placeholder="Search Your Product"
+        ></InputSearch>
+      </div>
+      <div className="w-full flex items-center gap-3 justify-start ">
+        <Button
+          name="Filters"
+          className="p-2 text-xl flex items-center gap-1"
+          onClick={() => setShowFilters((prev) => !prev)}
+        >
+          <CiBoxList />
+          Filters
+        </Button>
+        <div onClick={() => setSearchParams({})} className="clAllTxt">
+          Clear filters
         </div>
       </div>
       <div className="mt-4 gap-y-2 md:flex items-start justify-center md:gap-x-4">
-        <div className="basis-1/5 border-[2px] border-[#52ab98]">
-          {/* filter section */}
+        <div
+          className={`${
+            showFilters ? "hidden transform -translate-x-250" : "basis-1/5"
+          } border-[2px] border-[#52ab98] transition delay-100 duration-300 ease-in-out`}
+        >
           {filtersData?.map((item, id) => (
             <div key={item.id}>
               <hr />
@@ -46,7 +45,11 @@ const Products = () => {
             </div>
           ))}
         </div>
-        <div className="basis-4/5 flex flex-wrap gap-3 justify-center">
+        <div
+          className={`${
+            showFilters ? "" : "basis-4/5 "
+          }  flex flex-wrap gap-3 justify-center transition delay-100 duration-300 ease-in-out`}
+        >
           {products &&
             products.map((product) => (
               <ProductCard key={product._id} product={product}></ProductCard>
