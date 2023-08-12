37b196f16dfe6012f4b1e29b1ddb89c91d169b18
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-08-13 01:35:12 +0600

    [done] product catagory section

--- src/pages/FrontViewPages/pages/Home.js
+++ src/pages/FrontViewPages/pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import HeroSection from "../components/HeroSection";
 import ShowProducts from "../components/ShowProducts";
 import { products } from "../../../assets/ProductData/products";
+import ProductCategory from "../components/ProductCategory";
 
 const Home = () => {
   return (
     <div className="max-w-[80vw] mx-auto">
       <HeroSection />
       <ShowProducts randomProducts={products} />
+      <ProductCategory />
     </div>
   );
 };
